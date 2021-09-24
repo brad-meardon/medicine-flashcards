0585ac7423803cf29959a14ea644f6736c3b493f
Bradley Meardon <bradley.meardon@gmail.com>
2021-09-24 23:15:49 +0100

    Javascript completed
    The javascript has been completed for this with example questions that work.
    
    It will generate a random question, hide the answer and display the answer upon request
    
    Next to come is adding real life questions and answers that can be used for revision

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // }
 // Experimenting with JS and HTML
 
+alert("Click on the button to generate a question! Think of the answer and check if you were right!");
+
 var questionsAndAnswers = [
    {
       question: "What is the answer to this?",
@@ -11,8 +13,42 @@ var questionsAndAnswers = [
    {
       question: "What is your favourite food?",
       answer:"It's chicken!"
+   },
+   {
+      question: "What is your name?",
+      answer:"It's Brad!"
    }
 ]
 
-var randomQuestion = Math.floor(Math.random() * questionsAndAnswers.length);
-console.log(randomQuestion, questionsAndAnswers[randomQuestion]);
+// var randomQuestion = Math.floor(Math.random() * questionsAndAnswers.length);
+// console.log(randomQuestion, questionsAndAnswers[randomQuestion]);
+// Experimenting with random to generate random array selections
+
+
+
+// Generates a random selection from the questions and answers array, that cannot be the same as the previous
+function randomQuestionFunc(){
+   var arrayLength = questionsAndAnswers.length;
+   do{
+      var randomQuestion = Math.floor(Math.random() * arrayLength);
+   } while (randomQuestion === randomQuestionFunc.last);
+   randomQuestionFunc.last = randomQuestion;
+   return randomQuestion;
+}
+
+
+// Generates a random question using the above function, replaces the text on the page and hides the answer
+function generateQuestion(value){
+   var i = randomQuestionFunc();
+   var qMessage = questionsAndAnswers[i].question;
+   var aMessage = questionsAndAnswers[i].answer;
+   document.getElementById('qTxt').innerHTML = qMessage;
+   document.getElementById('aTxt').style.display="none";
+   document.getElementById('aTxt').innerHTML = aMessage;
+}
+
+
+// Shows the answer and displays it again
+function showAnswer(){
+   document.getElementById('aTxt').style.display="block";
+}
