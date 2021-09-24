4f53ba381a35d2c2a15b59de058182410cd67316
Bradley Meardon <bradley.meardon@gmail.com>
2021-09-24 22:44:37 +0100

    Updates to the site
    Optimized how responsive the site is
    
    Added a footer for credit
    
    Testing with Javascript for random selection from an array

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 //    document.getElementById('pText').innerHTML = "You pressed " + value;
 // }
 // Experimenting with JS and HTML
+
+var questionsAndAnswers = [
+   {
+      question: "What is the answer to this?",
+      answer: "It's 123!"
+   },
+   {
+      question: "What is your favourite food?",
+      answer:"It's chicken!"
+   }
+]
+
+var randomQuestion = Math.floor(Math.random() * questionsAndAnswers.length);
+console.log(randomQuestion, questionsAndAnswers[randomQuestion]);
