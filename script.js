f73eaf5a62b955e4e874def560f94c626e2c7076
Bradley Meardon <bradley.meardon@gmail.com>
2021-10-03 17:50:21 +0100

    Final touches
    Adding a background image for a better end user experience.
    
    Added 15 real life scenario flashcards for medicine studies.

--- script.js
+++ script.js
@@ -7,16 +7,64 @@ alert("Click on the button to generate a question! Think of the answer and check
 
 var questionsAndAnswers = [
    {
-      question: "What is the answer to this?",
-      answer: "It's 123!"
+      question: "Loss of sensation over the 'Sergeant's Badge' area would indicate injury of which nerve?",
+      answer: "The Axillary Nerve!"
    },
    {
-      question: "What is your favourite food?",
-      answer:"It's chicken!"
+      question: "What is contained within the carotid sheath?",
+      answer: "The carotid artery, INternal jugular vein and vagus nerve!"
    },
    {
-      question: "What is your name?",
-      answer:"It's Brad!"
+      question: "At which vertebarl level does the oesophagus pass through the diaphragm?",
+      answer: "T10 (10 letters in Oesophagus)!"
+   },
+   {
+      question: "What is the role of the chief cells in the stomache?",
+      answer: "They produce pepsinogen!"
+   },
+   {
+      question: "Where do the ureters constrict?",
+      answer: "1. Ureteropelvic Junction <br> 2. Crossing the common iliac artery <br> 3. Ureterovesical Junction with bladder"
+   },
+   {
+      question: "Which of the following hormones is secreted by the posterior pituitary glands? <br> a. FFH <br> b. ADH <br> c. Prolactin <br> d. TSH",
+      answer: "Correct answer is... B! ADH!"
+   },
+   {
+      question: "Multiple Sclerosis is an auto-immune disease. What is the cause and some common symptoms?",
+      answer: "Cause: The immune system attacks myelin and oligodendrocytes.<br>Symptoms: Fatigue, Muscle Weakness, Numbness, Bladder/Bowel problems and Speech/Swallowing issues."
+   },
+   {
+      question: "What is happening during Stage-2 of a cardiac myocyte's action potential?",
+      answer: "Plateau-calcium influx and transient potassium channels close."
+   },
+   {
+      question: "What is the difference between a CF potentiator and a CF corrector?",
+      answer: "Potentiator: They increase activity of defective CFTR at cell surface e.g. Ivacaftor.<br> Corrector: Increase trafficking of CFTR to plasma membrane e.g. Lumacaftor."
+   },
+   {
+      question: "Where on the nephron do thiazide diuretics act?",
+      answer: "In the DCT (They block the NA/CL cotransporter)."
+   },
+   {
+      question: "What muscles make up the levator ani?",
+      answer: "The pubococcygeus, puboroctalis and iliococcygeus."
+   },
+   {
+      question: "What is the main mechanism used by the immune system to kill parasites?",
+      answer: "Plasma cells produce anti-bodies, specifically IgE, which bind to the parasite. Eosinophils bind to the anti-bodies, using their FcεRI receptors. This activates Eosinophils and stimulates degranulation (anti-parasitic proteins are released)."
+   },
+   {
+      question: "What is the general role of the anterior compartment of the thigh and what muscles are found in this compartment?",
+      answer: "Role:Hip Flexion, Knee Extension<br>Muscles:  Iliopsoas (Psoas major+minor and iliacus)<br>Rectus femoris<br>Vastus (medialis, intermedialis and lateralis)<br>Pectineus<br>Sartorius<br> <br>*Vastus + Rectus = Quadriceps*"
+   },
+   {
+      question: "What is a normal blood pressue?",
+      answer: "Between 90/60mmHg and 120/80mmHg."
+   },
+   {
+      question: "What three shunts exist in fetal circulation?",
+      answer: "Ductus venosus (bypass liver), foramen ovale (RA -> LA), ductus arteriosus (PA -> Aorta)"
    }
 ]
 
